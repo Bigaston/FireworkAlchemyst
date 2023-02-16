7009d70cf2b28d0e9ed31720541e50271401e027
Bigaston <bigaston1@gmail.com>
2023-02-16 17:40:46 +0100

    Hand open and close for bin and trash

--- src/scene/Table.hx
+++ src/scene/Table.hx
@@ -7,23 +7,42 @@ class Table extends scene.Scene {
 	private var binBitmap:h2d.Bitmap;
 	private var bowlBitmap:h2d.Bitmap;
 
+	private var handClose:hxd.Cursor;
+	private var handOpen:hxd.Cursor;
+
 	public function new() {
 		super();
+
+		// Create custom cursors
+		handOpen = Custom(new hxd.Cursor.CustomCursor([hxd.Res.img.cursor.hand_open.toBitmap()], 0, 19, 23));
+		handClose = Custom(new hxd.Cursor.CustomCursor([hxd.Res.img.cursor.hand_close.toBitmap()], 0, 19, 23));
+
+		var globalInteractive = new h2d.Interactive(this.width, this.height, this);
+		globalInteractive.cursor = handOpen;
 
 		var tableTile = hxd.Res.img.table.toTile();
 		var tableBitmap = new h2d.Bitmap(tableTile, this);
 		tableBitmap.x = this.width / 2 - tableTile.width / 2;
 		tableBitmap.y = 230;
 
+		// Lighter
 		var lighterTile = hxd.Res.img.Lighter.toTile();
 		lighterBitmap = new h2d.Bitmap(lighterTile, this);
 		lighterBitmap.x = this.width / 4 - lighterTile.width / 2;
 		lighterBitmap.y = 35;
+		// + Lighter Interactive
+		var lighterInteractive = new h2d.Interactive(lighterTile.width, lighterTile.height, lighterBitmap);
+		lighterInteractive.cursor = handClose;
 
+		// Bin
 		var binTile = hxd.Res.img.Bin.toTile();
 		binBitmap = new h2d.Bitmap(binTile, this);
 		binBitmap.x = this.width / 4 * 3 - binTile.width / 2;
 		binBitmap.y = 55;
+
+		// + Bin Interactive
+		var binInteractive = new h2d.Interactive(binTile.width, binTile.height, binBitmap);
+		binInteractive.cursor = handClose;
 
 		var bowlTile = hxd.Res.img.Bowl.toTile();
 		bowlBitmap = new h2d.Bitmap(bowlTile, this);
