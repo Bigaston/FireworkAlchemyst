a74801d0304e9659e4ffae1f809bdb0ff80b3534
Bigaston <bigaston1@gmail.com>
2023-02-20 18:29:03 +0100

    Trail Effect Particle

---
+++ src/entity/TrailParticle.hx
@@ -0,0 +1,25 @@
+package entity;
+
+class TrailParticle extends entity.Particle {
+	private var frameUntilSpawn = 0.0;
+	private var frameBetweenSpawn = 0.0;
+	private var trailDuration = 10.0;
+
+	public function new(tile:h2d.Tile, x:Float, y:Float, color:h3d.Vector, direction:Vector2, lifetime:Float) {
+		super(tile, x, y, color, direction, lifetime);
+	}
+
+	override private function update(dt:Float):Bool {
+		this.frameUntilSpawn -= dt;
+
+		if (this.frameUntilSpawn <= 0) {
+			this.frameUntilSpawn = this.frameBetweenSpawn;
+
+			var particle = new entity.TrailEffectParticle(super.t, super.x, super.y, this.trailDuration);
+
+			super.batch.add(particle);
+		}
+
+		return super.update(dt);
+	}
+}
