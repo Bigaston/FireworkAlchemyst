3752d6631a8611a93f1917fde12ee7920672143e
Bigaston <bigaston1@gmail.com>
2023-02-21 09:56:24 +0100

    Firework spawn

--- src/scene/Firework.hx
+++ src/scene/Firework.hx
@@ -8,16 +8,22 @@ class Firework extends scene.Scene {
 	}
 
 	private var insideBowl = null;
+	private var handClose:hxd.Cursor;
+	private var handOpen:hxd.Cursor;
 
 	public function new(insideBowl:{
 		?color:String,
 		?type:String,
 		?modifier:String
 	}) {
+		handOpen = Custom(new hxd.Cursor.CustomCursor([hxd.Res.img.cursor.hand_open.toBitmap()], 0, 19, 23));
+		handClose = Custom(new hxd.Cursor.CustomCursor([hxd.Res.img.cursor.hand_close.toBitmap()], 0, 19, 23));
+
 		super();
 		this.insideBowl = insideBowl;
 
 		var globalInteractive = new h2d.Interactive(this.width, this.height, this);
+		globalInteractive.cursor = handOpen;
 
 		globalInteractive.onClick = function(e:hxd.Event) {
 			var particle:Class<entity.Particle>;
@@ -43,19 +49,33 @@ class Firework extends scene.Scene {
 			switch (this.insideBowl.type) {
 				case "circle":
 					firework = new entity.CircleFirework(this, new Vector2(Utils.random(200, 400), 500),
-						new Vector2(Utils.random(-10, 10), Utils.random(-15, -12)), Utils.random(30, 50), color, particle);
-				case "normal":
-					firework = new entity.Firework(this, new Vector2(Utils.random(200, 400), 500), new Vector2(Utils.random(-10, 10), Utils.random(-15, -12)),
+						new Vector2(Utils.random(-10, 10), Utils.random(-12, -8)), Utils.random(30, 50), color, particle);
+				case "cone":
+					firework = new entity.Firework(this, new Vector2(Utils.random(200, 400), 500), new Vector2(Utils.random(-10, 10), Utils.random(-12, -8)),
 						Utils.random(30, 50), color, particle);
 				case "vertical":
 					firework = new entity.VerticalFirework(this, new Vector2(Utils.random(200, 400), 500),
-						new Vector2(Utils.random(-10, 10), Utils.random(-15, -12)), Utils.random(30, 50), color, particle);
+						new Vector2(Utils.random(-10, 10), Utils.random(-12, -8)), Utils.random(30, 50), color, particle);
 				default:
-					firework = new entity.BengalFirework(this, new Vector2(Utils.random(200, 400), 500),
-						new Vector2(Utils.random(-10, 10), Utils.random(-15, -12)), Utils.random(30, 50), color, particle);
+					firework = new entity.BengalFirework(this, new Vector2(Utils.random(200, 400), 500), new Vector2(0, 0), Utils.random(30, 50), color,
+						particle);
 			}
 
 			this.elements.push(firework);
+		}
+
+		var arrowTile = hxd.Res.img.back_cursor.toTile();
+		var arrowBitmap = new h2d.Bitmap(arrowTile, this);
+		arrowBitmap.x = 20;
+		arrowBitmap.y = 20;
+
+		var arrowInteractive = new h2d.Interactive(arrowTile.width, arrowTile.height, arrowBitmap);
+		arrowInteractive.cursor = handClose;
+
+		arrowInteractive.onClick = function(e:hxd.Event):Void {
+			var scene = new scene.Table();
+
+			Game.instance.setScene(scene);
 		}
 	}
 
