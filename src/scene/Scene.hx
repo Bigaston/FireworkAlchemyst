619a54014e782994b90177fae8b970c39b4c2a1d
Bigaston <bigaston1@gmail.com>
2023-02-21 11:52:02 +0100

    Fix desktop build

--- src/scene/Scene.hx
+++ src/scene/Scene.hx
@@ -1,7 +1,15 @@
 package scene;
 
+import h2d.Scene;
+
 abstract class Scene extends h2d.Scene {
 	public var elements:Array<entity.Entity> = [];
+
+	public function new() {
+		super();
+
+		this.scaleMode = Fixed(650, 500, 1, Center, Center);
+	}
 
 	abstract public function update(dt:Float):Void;
 }
